074c6b0fb2ca7d9ebde1f90f66af675c24694338
German Reyes <oswaldo101.gr@gmail.com>
2025-11-01 19:04:44 -0600

    test: remove unused contextLoads test method from MonolithApplicationTests

--- backend/monolith/src/test/java/com/stocksync/backend/MonolithApplicationTests.java
+++ backend/monolith/src/test/java/com/stocksync/backend/MonolithApplicationTests.java
@@ -1,13 +1,7 @@
 package com.stocksync.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MonolithApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
