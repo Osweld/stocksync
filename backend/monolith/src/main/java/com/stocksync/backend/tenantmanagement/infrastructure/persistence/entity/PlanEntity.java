38b9c6f5e9bf3570cfe765500ab19000bde98578
German Reyes <oswaldo101.gr@gmail.com>
2025-11-06 23:30:47 -0600

    feat: add PlanEntity class for tenant management persistence

---
+++ backend/monolith/src/main/java/com/stocksync/backend/tenantmanagement/infrastructure/persistence/entity/PlanEntity.java
@@ -0,0 +1,30 @@
+package com.stocksync.backend.tenantmanagement.infrastructure.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "plans")
+public class PlanEntity {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "descripcion", length = 1000)
+    private String descripcion;
+
+}
