585067022e01fad6363be682aa653d19af7c88a6
German Reyes <oswaldo101.gr@gmail.com>
2025-11-07 23:38:01 -0600

    feat: add getDefaultUserStatus method to TenantDefaultsPolicy interface

--- backend/monolith/src/main/java/com/stocksync/backend/tenantmanagement/application/port/out/TenantDefaultsPolicy.java
+++ backend/monolith/src/main/java/com/stocksync/backend/tenantmanagement/application/port/out/TenantDefaultsPolicy.java
@@ -2,8 +2,10 @@ package com.stocksync.backend.tenantmanagement.application.port.out;
 
 import com.stocksync.backend.tenantmanagement.domain.model.PlanId;
 import com.stocksync.backend.tenantmanagement.domain.model.TenantStatus;
+import com.stocksync.backend.tenantmanagement.domain.model.UserStatus;
 
 public interface TenantDefaultsPolicy {
     PlanId getDefaultPlanId(); 
     TenantStatus getInitialStatus();
+    UserStatus getDefaultUserStatus();
 }
