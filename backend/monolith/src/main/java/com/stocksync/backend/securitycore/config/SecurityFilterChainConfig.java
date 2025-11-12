6bed0b2d3399d4932e0da4baad489f1ff5126f33
German Reyes <oswaldo101.gr@gmail.com>
2025-11-11 22:45:39 -0600

    refactor: remove unused import for SecurityProperties.User in SecurityFilterChainConfig

--- backend/monolith/src/main/java/com/stocksync/backend/securitycore/config/SecurityFilterChainConfig.java
+++ backend/monolith/src/main/java/com/stocksync/backend/securitycore/config/SecurityFilterChainConfig.java
@@ -1,6 +1,5 @@
 package com.stocksync.backend.securitycore.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
